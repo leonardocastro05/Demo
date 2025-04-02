0e39786fba2c033e1c9dcebcc694c731ff8c0d0e
Leonardo <leonardo.decastro@insbaixcamp.cat>
2025-04-02 13:53:14 +0200

    Afegim tercera línia

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Afegim una segona línia");
+        System.out.println("Afegim una tercera línia");
     }
 }
